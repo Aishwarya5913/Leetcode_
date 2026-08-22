6f6b66e24a8f6f233d23ca68ae1cd5ec2fa8c240
Aishwarya Kute <aishukute2000@gmail.com>
2026-08-22 22:30:05 +0530

    Time: 1 ms (26.40%) | Memory: 44.4 MB (9.16%) - LeetSync

---
+++ 88-merge-sorted-array/merge-sorted-array.java
@@ -0,0 +1,49 @@
+class Solution {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        for(int i =0; i<n; i++){
+            nums1[m+i]=nums2[i];
+        }
+        divide(nums1,0,m+n-1);
+    }
+    public void divide(int[] nums1, int low, int high){
+        if(low<high){
+            int mid = low+(high-low)/2;
+            divide(nums1,low,mid);
+            divide(nums1,mid+1,high);
+            Merge(nums1, low, mid, high);
+            
+        }
+    }
+    public void Merge(int nums1[], int low, int mid, int high){
+        ArrayList<Integer> temp = new ArrayList<>();
+        int left = low;
+        int right = mid+1;
+        while(left<=mid && right<=high){
+            if(nums1[left]<=nums1[right]){
+                temp.add(nums1[left]);
+                left++;
+            }
+            else
+            {
+                temp.add(nums1[right]);
+                right++;
+            }
+        }
+        while(left<=mid){
+            temp.add(nums1[left]);
+            left++;
+        }
+        while(right<=high)
+        {
+            temp.add(nums1[right]);
+            right++;
+        }
+        for(int i=low;i<=high;i++){
+            nums1[i]=temp.get(i-low);
+        }
+    }
+} 
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
