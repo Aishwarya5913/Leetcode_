c112e2fdf895d9cb65e97de7f10516146402390f
Aishwarya Kute <aishukute2000@gmail.com>
2026-08-26 14:50:03 +0530

    Time: 312 ms (80.62%) | Memory: 0B (100.00%) - LeetSync

---
+++ 602-friend-requests-ii-who-has-the-most-friends/friend-requests-ii-who-has-the-most-friends.sql
@@ -0,0 +1,6 @@
+select requester_id as id , count(requester_id) as num from 
+(select requester_id from requestaccepted
+ union all 
+select accepter_id from requestaccepted ) as temp
+group by id
+order by num desc limit 1;
