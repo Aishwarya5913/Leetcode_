01b49df59de52afa2401cf2856d984fe98893636
Aishwarya Kute <aishukute2000@gmail.com>
2026-09-11 19:54:54 +0530

    Time: 0 ms (100.00%) | Memory: 43 MB (34.26%) - LeetSync

--- 742-to-lower-case/to-lower-case.java
+++ 742-to-lower-case/to-lower-case.java
@@ -1,5 +1,11 @@
 class Solution {
     public String toLowerCase(String s) {
-        return s.toLowerCase();
+        char[] result = s.toCharArray();
+        for(int i =0; i<result.length;i++){
+            if('A'<=result[i]&&result[i]<='Z')
+                result[i]= (char) (result[i]-'A'+'a');
+
+        }
+        return new String(result);
     }
 }
