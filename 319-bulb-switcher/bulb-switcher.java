ab27c5e1393511723a605d1a865209b2059cc42b
Aishwarya Kute <aishukute2000@gmail.com>
2026-09-17 11:57:37 +0530

    Time: 0 ms (100.00%) | Memory: 42.4 MB (15.93%) - LeetSync

--- 319-bulb-switcher/bulb-switcher.java
+++ 319-bulb-switcher/bulb-switcher.java
@@ -3,8 +3,7 @@ class Solution {
         int i =1, on =-1,k=0;
         if(n==0)
             return 0;
-        if(n==1||n==2||n==3)
-            return 1;
+        
         else{
             while(k<=n){
                 k =(int) (Math.pow(i,2));
